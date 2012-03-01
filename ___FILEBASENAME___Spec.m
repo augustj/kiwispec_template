38dc8cfd3730e1ba029aa4ea1d08c61acac159e6
August Jaenicke <august@carbonfive.com>
2012-03-01 09:35:00 -0800

    First commit

---
+++ ___FILEBASENAME___Spec.m
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "___FILEBASENAMEASIDENTIFIER___.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+  
+});
+
+SPEC_END
